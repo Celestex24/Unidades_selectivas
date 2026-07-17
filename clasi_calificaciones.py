cacb7107c7940859dac2689c06b2eab161f01032
Celestex24 <glzcelestex@gmail.com>
2026-07-17 13:02:11 -0600

    Clasificar calificaciones

---
+++ clasi_calificaciones.py
@@ -0,0 +1,13 @@
+# Clasificar calificaciones
+nota = float(input("Ingrese calificacion (0-100): "))
+if nota >= 90:
+    letra = "A"
+if nota >= 80:
+    letra = "B"
+elif nota >= 70:
+    letra = "C"
+elif nota >= 60:
+    letra = "D"
+else:
+    letra = "F"
+print("La calificacion es: ", letra)
